4af08131ccfd57b0f9591904d8e9f5d3999e81e3
Vladyslav Pyrch <vlad.nuron@gmail.com>
2024-06-04 20:18:08 +0200

    test

---
+++ src/Endpoint.cs
@@ -0,0 +1,59 @@
+﻿namespace codecrafters_http_server;
+
+public delegate byte[] EndpointAction(byte[] request);
+
+public class Endpoint
+{
+    private string _path;
+
+    private HttpMethod _method;
+
+    private EndpointAction _endpointAction;
+
+    public Endpoint(string path, HttpMethod method, EndpointAction endpointAction)
+    {
+        Path = path;
+        Method = method;
+        EndpointAction = endpointAction;
+    }
+    
+    public Endpoint(string path, string method, EndpointAction endpointAction)
+    {
+        Path = path;
+        Method = HttpMethod.Parse(method);
+        EndpointAction = endpointAction;
+    } 
+
+    public string Path
+    {
+        get => _path;
+        private set
+        {
+            if (value is null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            bool isValid = Uri.TryCreate(value, UriKind.Relative, out Uri? _);
+
+            if (!isValid)
+            {
+                throw new InvalidOperationException();
+            }
+
+            _path = value;
+        }
+    }
+
+    public HttpMethod Method
+    {
+        get => _method;
+        private set => _method = value ?? throw new InvalidOperationException();
+    }
+
+    public EndpointAction EndpointAction
+    {
+        get => _endpointAction;
+        set => _endpointAction = value ?? throw new InvalidOperationException();
+    }
+}
--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,8 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
+using codecrafters_http_server;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -10,18 +12,39 @@ var server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 Socket socket = server.AcceptSocket(); // wait for client
 
-var buffer = new byte[1024];
-
+var buffer= new byte[1024];
 await socket.ReceiveAsync(buffer);
-
 string request = Encoding.UTF8.GetString(buffer);
 
 Console.WriteLine(request);
 
-string[] requestLines = request.Split("\n\r");
+string[] startLine = request.Split("\r\n")[0].Split(' ');
 
-bool validEndpoint = requestLines[0].Split(' ')[1].Equals("/");
+List<Endpoint> endpoints =
+[
+    new Endpoint("/echo/", HttpMethod.Get, requestBytes =>
+    {
+        string requestString = Encoding.UTF8.GetString(requestBytes);
+        Match match = Regex.Match(requestString, @"(?<=/echo/)[^\s]+");
 
-string response = validEndpoint ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 Not Found\r\n\r\n";
+        return Encoding.UTF8.GetBytes($"HTTP/1.1 200 OK\r\n" +
+                                      $"Content-Type: text/plain\r\n" +
+                                      $"Content-Length: {match.Value.Length}\r\n" +
+                                      $"\r\n" +
+                                      $"{match.Value}");
+    })
+];
 
-await socket.SendAsync(Encoding.UTF8.GetBytes(response));
+Endpoint? endpoint = endpoints
+    .FirstOrDefault(
+        e => startLine[1].StartsWith(e.Path) && e.Method == HttpMethod.Parse(startLine[0]));
+
+if (endpoint is null)
+{
+    await socket.SendAsync("HTTP/1.1 404 Not Found\r\n\r\n"u8.ToArray());
+}
+else
+{
+    await socket.SendAsync(endpoint.EndpointAction.Invoke(buffer));
+}
+    
