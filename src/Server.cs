99707773c951d2268521aa431d3dcfe912c794a4
Vladyslav Pyrch <vlad.nuron@gmail.com>
2024-06-04 18:50:24 +0200

    test

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -9,4 +10,18 @@ var server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 Socket socket = server.AcceptSocket(); // wait for client
 
-await socket.SendAsync("HTTP/1.1 200 OK\r\n\r\n"u8.ToArray());
+var buffer = new byte[1024];
+
+await socket.ReceiveAsync(buffer);
+
+string request = Encoding.UTF8.GetString(buffer);
+
+Console.WriteLine(request);
+
+string[] requestLines = request.Split("\n\r");
+
+bool validEndpoint = requestLines[0].Split(' ')[1].Equals("/");
+
+string response = validEndpoint ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 NotFound\r\n\r\n";
+
+await socket.SendAsync(Encoding.UTF8.GetBytes(response));
