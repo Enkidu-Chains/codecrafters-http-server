9da67ddaf38d55a2513483eb52b8dfdb411169cf
Vladyslav Pyrch <vlad.nuron@gmail.com>
2024-06-10 23:22:13 +0200

    test

--- src/Endpoint.cs
+++ src/Endpoint.cs
@@ -1,59 +1,38 @@
-﻿namespace codecrafters_http_server;
-
-public delegate byte[] EndpointAction(byte[] request);
-
-public class Endpoint
-{
-    private string _path;
-
-    private HttpMethod _method;
-
-    private EndpointAction _endpointAction;
-
-    public Endpoint(string path, HttpMethod method, EndpointAction endpointAction)
-    {
-        Path = path;
-        Method = method;
-        EndpointAction = endpointAction;
-    }
-    
-    public Endpoint(string path, string method, EndpointAction endpointAction)
-    {
-        Path = path;
-        Method = HttpMethod.Parse(method);
-        EndpointAction = endpointAction;
-    } 
-
-    public string Path
-    {
-        get => _path;
-        private set
-        {
-            if (value is null)
-            {
-                throw new InvalidOperationException();
-            }
-
-            bool isValid = Uri.TryCreate(value, UriKind.Relative, out Uri? _);
-
-            if (!isValid)
-            {
-                throw new InvalidOperationException();
-            }
-
-            _path = value;
-        }
-    }
-
-    public HttpMethod Method
-    {
-        get => _method;
-        private set => _method = value ?? throw new InvalidOperationException();
-    }
-
-    public EndpointAction EndpointAction
-    {
-        get => _endpointAction;
-        set => _endpointAction = value ?? throw new InvalidOperationException();
-    }
-}
+﻿// namespace codecrafters_http_server;
+//
+// public delegate byte[] EndpointAction(byte[] request);
+//
+// // TODO: smart endpoint path (or endpoint regex)
+// public class Endpoint
+// {
+//
+//     private HttpMethod _method;
+//
+//     private EndpointAction _endpointAction;
+//
+//     public Endpoint(string path, HttpMethod method, EndpointAction endpointAction)
+//     {
+//         Path = path;
+//         Method = method;
+//         EndpointAction = endpointAction;
+//     }
+//     
+//     public Endpoint(string path, string method, EndpointAction endpointAction)
+//     {
+//         Path = path;
+//         Method = HttpMethod.Parse(method);
+//         EndpointAction = endpointAction;
+//     }
+//
+//     public HttpMethod Method
+//     {
+//         get => _method;
+//         private set => _method = value ?? throw new InvalidOperationException();
+//     }
+//
+//     public EndpointAction EndpointAction
+//     {
+//         get => _endpointAction;
+//         set => _endpointAction = value ?? throw new InvalidOperationException();
+//     }
+// }
--- src/Server.cs
+++ src/Server.cs
@@ -2,7 +2,6 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
-using codecrafters_http_server;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -15,36 +14,29 @@ Socket socket = server.AcceptSocket(); // wait for client
 var buffer= new byte[1024];
 await socket.ReceiveAsync(buffer);
 string request = Encoding.UTF8.GetString(buffer);
+byte[] response;
 
 Console.WriteLine(request);
 
 string[] startLine = request.Split("\r\n")[0].Split(' ');
 
-List<Endpoint> endpoints =
-[
-    new Endpoint("/echo/", HttpMethod.Get, requestBytes =>
-    {
-        string requestString = Encoding.UTF8.GetString(requestBytes);
-        Match match = Regex.Match(requestString, @"(?<=/echo/)[^\s]+");
-
-        return Encoding.UTF8.GetBytes($"HTTP/1.1 200 OK\r\n" +
-                                      $"Content-Type: text/plain\r\n" +
-                                      $"Content-Length: {match.Value.Length}\r\n" +
-                                      $"\r\n" +
-                                      $"{match.Value}");
-    }),
-    new Endpoint("/", HttpMethod.Get, requestBytes => Encoding.UTF8.GetBytes($"HTTP/1.1 200 OK\r\n\r\n"))
-];
-
-Endpoint? endpoint = endpoints
-    .FirstOrDefault(e => startLine[1].StartsWith(e.Path) && e.Method == HttpMethod.Parse(startLine[0]));
-
-if (endpoint is null)
+if (startLine[1].StartsWith("/echo/"))
 {
-    await socket.SendAsync("HTTP/1.1 404 Not Found\r\n\r\n"u8.ToArray());
+    Match match = Regex.Match(startLine[1], @"(?<=/echo/)[^\s/]+");
+
+    response = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n" +
+                                             "Content-Type: text/plain\r\n" +
+                                             $"Content-Length: {match.Value.Length}\r\n" +
+                                             "\r\n" +
+                                             $"{match.Value}");
+}
+else if (startLine[1] == "/")
+{
+    response = "HTTP/1.1 200 OK\r\n\r\n"u8.ToArray();
 }
 else
 {
-    await socket.SendAsync(endpoint.EndpointAction.Invoke(buffer));
+    response = "HTTP/1.1 404 Not Found\r\n\r\n"u8.ToArray();
 }
-    
+
+await socket.SendAsync(response);
