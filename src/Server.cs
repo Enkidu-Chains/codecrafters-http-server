5a97ba3808f522f03288afa73cb786cbfbd18808
Vladyslav Pyrch <vlad.nuron@gmail.com>
2024-06-11 00:32:51 +0200

    test

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,5 @@
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Text.RegularExpressions;
 using codecrafters_http_server;
 
@@ -10,39 +9,42 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 var server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-Socket socket = server.AcceptSocket(); // wait for client
 
-var requestBytes = new byte[10*1024];
-int byteReceived = await socket.ReceiveAsync(requestBytes);
+while (true)
+{
+    Socket socket = server.AcceptSocket(); // wait for client
+    var requestBytes = new byte[10*1024];
+    int byteReceived = await socket.ReceiveAsync(requestBytes);
 
-var request = new HttpRequest(requestBytes);
-HttpResponse response;
+    var request = new HttpRequest(requestBytes);
+    HttpResponse response;
 
-if (request.Path.StartsWith("/echo/"))
-{
-    Match match = Regex.Match(request.Path, @"(?<=/echo/)[^\s/]+");
+    if (request.Path.StartsWith("/echo/"))
+    {
+        Match match = Regex.Match(request.Path, @"(?<=/echo/)[^\s/]+");
 
-    response = new HttpResponse("HTTP/1.1", 200, "OK")
-        .AddHeader("Content-Type", "text/plain")
-        .AddHeader("Content-Length", $"{match.Value.Length}");
-    response.Body = $"{match.Value}";
-}
-else if (request.Path == "/user-agent")
-{
-    string userAgent = request.Headers["User-Agent"];
-    
-    response = new HttpResponse("HTTP/1.1", 200, "OK")
-        .AddHeader("Content-Type", "text/plain")
-        .AddHeader("Content-Length", userAgent.Length.ToString());
-    response.Body = userAgent;
-}
-else if (request.Path == "/")
-{
-    response = new HttpResponse("HTTP/1.1", 200, "OK");
-}
-else
-{
-    response = new HttpResponse("HTTP/1.1", 404, "Not Found");
-}
+        response = new HttpResponse("HTTP/1.1", 200, "OK")
+            .AddHeader("Content-Type", "text/plain")
+            .AddHeader("Content-Length", $"{match.Value.Length}");
+        response.Body = $"{match.Value}";
+    }
+    else if (request.Path == "/user-agent")
+    {
+        string userAgent = request.Headers["User-Agent"];
+        
+        response = new HttpResponse("HTTP/1.1", 200, "OK")
+            .AddHeader("Content-Type", "text/plain")
+            .AddHeader("Content-Length", userAgent.Length.ToString());
+        response.Body = userAgent;
+    }
+    else if (request.Path == "/")
+    {
+        response = new HttpResponse("HTTP/1.1", 200, "OK");
+    }
+    else
+    {
+        response = new HttpResponse("HTTP/1.1", 404, "Not Found");
+    }
 
 await socket.SendAsync(response.ToBytes());
+}
